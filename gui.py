d1830e6f2a01c091e090fd85461566c6cca4342b
Simon Schork <simon.schork@gmail.com>
2020-05-10 13:00:01 +0200

    added if __name__ == "__main__" block

--- gui.py
+++ gui.py
@@ -56,67 +56,59 @@ def stop():
     )
 
 
-# Create window
-app = Tk()
+if __name__ == "__main__":
+    # Create window
+    app = Tk()
 
+    # Format window
+    app.title("Track your worktime")
 
-# Format window
-app.title("Track your worktime")
+    # Start button
+    start_button = Button(app, text="Start", command=start, height=2, width=10)
+    start_button.grid(row=0, column=0)
 
+    # Pause button
+    pause_button = Button(app, text="Pause", command=pause, height=2, width=10)
+    pause_button.grid(row=0, column=1)
 
-# Start button
-start_button = Button(app, text="Start", command=start, height=2, width=10)
-start_button.grid(row=0, column=0)
+    # Resume button
+    resume_button = Button(
+        app, text="Resume", command=resume, height=2, width=10)
+    resume_button.grid(row=0, column=2)
 
+    # Stop button
+    stop_button = Button(app, text="Stop", command=stop, height=2, width=10)
+    stop_button.grid(row=0, column=3)
 
-# Pause button
-pause_button = Button(app, text="Pause", command=pause, height=2, width=10)
-pause_button.grid(row=0, column=1)
+    # Row labels
+    worktime_label = Label(app, text="Worktime:")
+    worktime_label.grid(row=2, column=0)
+    pause_label = Label(app, text="Pause:")
+    pause_label.grid(row=3, column=0)
 
+    # Start column
+    start_label = Label(app, text="Start")
+    start_label.grid(row=1, column=1)
+    worktime_start_value = Label(app, text="")
+    worktime_start_value.grid(row=2, column=1)
+    pause_start_value = Label(app, text="")
+    pause_start_value.grid(row=3, column=1)
 
-# Resume button
-resume_button = Button(app, text="Resume", command=resume, height=2, width=10)
-resume_button.grid(row=0, column=2)
+    # Stop column
+    stop_label = Label(app, text="Stop")
+    stop_label.grid(row=1, column=2)
+    worktime_stop_value = Label(app, text="")
+    worktime_stop_value.grid(row=2, column=2)
+    pause_stop_value = Label(app, text="")
+    pause_stop_value.grid(row=3, column=2)
 
+    # Duration column
+    duration_label = Label(app, text="Duration")
+    duration_label.grid(row=1, column=3)
+    worktime_duration_value = Label(app, text="")
+    worktime_duration_value.grid(row=2, column=3)
+    pause_duration_value = Label(app, text="")
+    pause_duration_value.grid(row=3, column=3)
 
-# Stop button
-stop_button = Button(app, text="Stop", command=stop, height=2, width=10)
-stop_button.grid(row=0, column=3)
-
-
-# Row labels
-worktime_label = Label(app, text="Worktime:")
-worktime_label.grid(row=2, column=0)
-pause_label = Label(app, text="Pause:")
-pause_label.grid(row=3, column=0)
-
-
-# Start column
-start_label = Label(app, text="Start")
-start_label.grid(row=1, column=1)
-worktime_start_value = Label(app, text="")
-worktime_start_value.grid(row=2, column=1)
-pause_start_value = Label(app, text="")
-pause_start_value.grid(row=3, column=1)
-
-
-# Stop column
-stop_label = Label(app, text="Stop")
-stop_label.grid(row=1, column=2)
-worktime_stop_value = Label(app, text="")
-worktime_stop_value.grid(row=2, column=2)
-pause_stop_value = Label(app, text="")
-pause_stop_value.grid(row=3, column=2)
-
-
-# Duration column
-duration_label = Label(app, text="Duration")
-duration_label.grid(row=1, column=3)
-worktime_duration_value = Label(app, text="")
-worktime_duration_value.grid(row=2, column=3)
-pause_duration_value = Label(app, text="")
-pause_duration_value.grid(row=3, column=3)
-
-
-# Start app
-app.mainloop()
+    # Start app
+    app.mainloop()
